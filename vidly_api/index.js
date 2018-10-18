2b4f9e359eb11814d9e16eaf601040b3c48bb17b
jellythewobbly <jellythewobbly@hotmail.com>
2018-10-18 15:49:30 +0800

    added async error handling

--- vidly_api/index.js
+++ vidly_api/index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const config = require('config');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -11,6 +12,7 @@ const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const home = require('./routes/home');
+const error = require('./middleware/error');
 
 const app = express();
 
@@ -37,6 +39,7 @@ app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/', home);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}......`));
